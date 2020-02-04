6aa9adbd4f52b8d2fe220f071946f73491aba3dd
Diego Faria <diego.rdfaria@gmail.com>
2020-02-04 00:36:05 -0300

    CRUD Scaffolding

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -2,8 +2,8 @@
 {
     public class Department
     {
-        public int Id { get; }
-        public string Name { get; }
+        public int Id { get; set; }
+        public string Name { get; set; }
 
         public Department(int id, string name)
         {
